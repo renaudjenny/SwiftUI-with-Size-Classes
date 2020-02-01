45adde2c0d0f2ef94a175158aa2b206526d01f54
Renaud Jenny <renaud.jenny@gmail.com>
2020-02-01 08:44:31 +0000

    Initial Commit

---
+++ SwiftUI with Size Classes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI with Size Classes
+//
+//  Created by Renaud JENNY on 01/02/2020.
+//  Copyright © 2020 Renaud JENNY. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
